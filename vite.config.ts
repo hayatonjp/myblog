9a2473c039ca8674890182c0a3851e7f11f98130
haya573 <nakasai.hayato@gmail.com>
2025-10-16 01:38:35 +0900

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import mdx from '@mdx-js/rollup';
-import adapter from "@hono/vite-dev-server/node";
-import build from "@hono/vite-build/node";
 import honox from 'honox/vite';
 import pages from '@hono/vite-cloudflare-pages'
 import remarkFrontmatter from 'remark-frontmatter';
@@ -9,20 +7,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
     return {
-      build: {
-        emptyOutDir: false,
-      },
       plugins: [
-        honox({
-          devServer: { adapter },
-          client: { input: ["./app/style.css"] },
-        }),
+        honox(),
         pages(),
         mdx({
           jsxImportSource: 'hono/jsx',
           remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
         }),
-        build(),
       ],
     };
 });
