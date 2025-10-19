70d578a06ac8c3d76a3c7e8a1ca283ad35b1747f
haya573 <nakasai.hayato@gmail.com>
2025-10-19 23:20:37 +0900

    tweak

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,23 +3,31 @@ import { Meta } from './types';
 
 export default createRoute((c) => {
   const posts = import.meta.glob<{ frontmatter: Meta }>('./posts/*.mdx', { eager: true })
+  
+  // 投稿を日付の降順でソート
+  const sortedPosts = Object.entries(posts)
+    .filter(([_, module]) => module.frontmatter)
+    .sort(([_, a], [__, b]) => {
+      const dateA = new Date(a.frontmatter!.date)
+      const dateB = new Date(b.frontmatter!.date)
+      return dateB.getTime() - dateA.getTime() // 降順
+    })
+  
   return c.render(
     <div class="container is-max-desktop mt-6 px-5 fixed-grid">
       <h1 class="is-size-3 mb-2">記事一覧</h1>
       <div>
-        {Object.entries(posts).map(([id, module]) => {
-          if (module.frontmatter) {
-            return (
-              <div class="has-background-white p-4 is-rounded box is-shadowless">
-                <a href={`${id.replace(/\.mdx$/, '')}`} class="is-flex is-align-items-center">
-                  <div class="ml-4">
-                    <p class="is-size-4 has-text-black">{module.frontmatter.title}</p>
-                    <p class="has-text-black">{module.frontmatter.date}</p>
-                  </div>
-                </a>
-              </div>
-            );
-          }
+        {sortedPosts.map(([id, module]) => {
+          return (
+            <div class="has-background-white p-4 is-rounded box is-shadowless">
+              <a href={`${id.replace(/\.mdx$/, '')}`} class="is-flex is-align-items-center">
+                <div class="ml-4">
+                  <p class="is-size-4 has-text-black">{module.frontmatter!.title}</p>
+                  <p class="has-text-black">{module.frontmatter!.date}</p>
+                </div>
+              </a>
+            </div>
+          );
         })}
       </div>
     </div>
